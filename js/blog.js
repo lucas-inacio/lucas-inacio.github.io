01ab7a7ac088e2b304fa42ec26c0ef0efe5e5cc1
lucas-inacio <lucas-inacio@users.noreply.github.com>
2021-05-08 03:54:58 +0000

    deploy: 59e1cd0465f7f98081cb584eaf38fbf0f119f86e

--- js/blog.js
+++ js/blog.js
@@ -1,11 +1,39 @@
+function hideSideBar() {
+    $('.auto-hide-click').each(function () {
+        $(this).addClass('collapsed');
+        this.setAttribute('aria-expanded', 'false');
+    });
+
+    $('div[data-parent="#sidebar-accordion"]').removeClass('show');
+}
+
 $(document).ready(() => {
     let menu = $('.auto-hide')[0];
     menu.style.visibility = 'hidden';
     
-    $(window).scroll((e) => {
-        if (window.scrollY === 0)
+    $(document).scroll((e) => {
+        if (window.scrollY === 0) {
             menu.style.visibility = 'hidden';
+        }
         else
             menu.style.visibility = 'visible';
-    })
+    
+        hideSideBar();
+    });
+
+    window.addEventListener(
+        'click',
+        (e) => {
+            let sidebar = $('#sidebar-accordion').first();
+            let pos = sidebar.position();
+            let width = sidebar.outerWidth();
+            let height = sidebar.outerHeight();
+            if (e.clientX < pos.left || e.clientX > pos.left + width ||
+                e.clientY < pos.top || e.clientY > pos.top + height) {
+
+                hideSideBar();
+            }
+        },
+        true
+    );
 });
